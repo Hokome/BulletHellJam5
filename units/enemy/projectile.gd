33cd7eed65977210ab859242c9a041f7d4a6ac67
Hokome <m.k.esseghir@protonmail.com>
2024-05-11 15:04:46 +0200

    Bullet lifetime

--- units/enemy/projectile.gd
+++ units/enemy/projectile.gd
@@ -3,6 +3,12 @@ extends Area2D
 @export var speed: float
 @export var faction: Battle.Faction
 @export var damage := 1
+@export var lifetime: float = 20
+
+func _ready():
+	var timer := time_manager.create_timer(lifetime)
+	add_child(timer)
+	timer.timeout.connect(queue_free)
 
 func _physics_process(delta):
 	translate(transform.basis_xform(Vector2.RIGHT) * delta * speed)
