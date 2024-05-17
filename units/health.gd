e3e28a07b52c92240c3fb5d1928072524a3863ff
Hokome <m.k.esseghir@protonmail.com>
2024-05-17 12:28:31 +0200

    Enemy health bar

--- units/health.gd
+++ units/health.gd
@@ -2,15 +2,24 @@ class_name Health extends Node2D
 
 signal on_death()
 
+@export var ui: ProgressBar
 @export var max_value: int
+
 var value: int:
 	set(val):
 		value = min(max_value, val)
+		if ui:
+			ui.value = value
 		if value <= 0:
 			kill()
 
 func _ready():
-	value = max_value
+	if ui:
+		ui.max_value = max_value
+	
+	if value == 0:
+		value = max_value
+	
 
 func kill():
 	on_death.emit()
